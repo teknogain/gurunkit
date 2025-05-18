f4dbdc8a8cddc166b367f886cb5973a4d01e813e
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-05-18 17:29:29 +0700

    requirement page

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,10 +15,7 @@ export default defineConfig({
 				},
 				{
 					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
+					autogenerate: { directory: 'components' }
 				},
 				{
 					label: 'Reference',
